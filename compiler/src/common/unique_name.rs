c15d9ccde8bb94a3ed49ea1e9d836450396e316a
Waterlens <waterlens@waterlens.moe>
2022-06-12 22:43:11 +0800

    remove  a warning

--- compiler/src/common/unique_name.rs
+++ compiler/src/common/unique_name.rs
@@ -13,10 +13,10 @@ impl UniqueName {
         if self.history.contains_key(base) {
             let n = self.history.get_mut(base).unwrap();
             *n += 1;
-            return format!("{}.{}", base, n);
+            format!("{}.{}", base, n)
         } else {
             self.history.insert(base.to_string(), 0);
-            return format!("{}.0", base);
+            format!("{}.0", base)
         }
     }
 }
